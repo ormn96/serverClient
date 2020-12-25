53ebec723fd9a565d0a291cbe6a542512eb24d0e
or man <Ormn1996@gmail.com>
2020-12-25 11:00:54 +0200

    fix npm

--- myTest.js
+++ myTest.js
@@ -3,7 +3,7 @@ let port = process.env.PORT || 3000;
 let app = express();
 
 const {Pool, Client} = require('pg')
-const connectionString = 'postgres://abpqsvrhxskjrr:02d82386ab2e881c39b2aa7b1861d3caff197f7965f927f81fb354d19887713a@ec2-176-34-123-50.eu-west-1.compute.amazonaws.com:5432/ddlsie8d46flhp'
+const connectionString = 'postgressql://abpqsvrhxskjrr:02d82386ab2e881c39b2aa7b1861d3caff197f7965f927f81fb354d19887713a@ec2-176-34-123-50.eu-west-1.compute.amazonaws.com:5432/ddlsie8d46flhp'
 
 const client = new Client({
   connectionString:connectionString
@@ -12,7 +12,7 @@ const client = new Client({
 client.connect()
 
 
-client.query('select * from links', (err, res)=>{
+client.query('select * from public.links', (err, res)=>{
   console.log(err, res)
   client.end()
 })
