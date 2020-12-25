9b3993491df27d4e66ac3e430f17b90f8f3fb789
or man <Ormn1996@gmail.com>
2020-12-25 11:26:41 +0200

    fix npm

--- myTest.js
+++ myTest.js
@@ -5,9 +5,18 @@ let app = express();
 const {Pool, Client} = require('pg')
 const connectionString = 'postgressql://abpqsvrhxskjrr:02d82386ab2e881c39b2aa7b1861d3caff197f7965f927f81fb354d19887713a@ec2-176-34-123-50.eu-west-1.compute.amazonaws.com:5432/ddlsie8d46flhp'
 
+// const client = new Client({
+//   connectionString:connectionString
+// })
+
 const client = new Client({
-  connectionString:connectionString
-})
+    user: "abpqsvrhxskjrr",
+    password: "02d82386ab2e881c39b2aa7b1861d3caff197f7965f927f81fb354d19887713a",
+    database: "ddlsie8d46flhp",
+    port: 5432,
+    host: "ec2-176-34-123-50.eu-west-1.compute.amazonaws.com",
+    ssl: true
+}); 
 
 client.connect()
 
